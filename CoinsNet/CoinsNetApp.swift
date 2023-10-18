054856ab28997deda21cd6f9fa00378286b10c3d
Vitor Walcker <vitor0321@gmail.com>
2023-10-18 17:32:41 -0300

    Initial Commit

---
+++ CoinsNet/CoinsNetApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinsNetApp.swift
+//  CoinsNet
+//
+//  Created by vitor.walcker on 18/10/23.
+//
+
+import SwiftUI
+
+@main
+struct CoinsNetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
